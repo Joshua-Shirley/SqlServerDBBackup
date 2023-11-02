4279e11a577c6178de2280e99296aaba977b99d7
Josh Shirley <jshir28@wgu.edu>
2023-11-02 16:46:26 -0600

    Initial Commit

---
+++ Database Backup/Procedure.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Backup
+{
+    internal class Procedure
+    {
+        private readonly static string connString = "Server=.;Database=master;Trusted_Connection=True;TrustServerCertificate=True";
+        internal static void BackupAll ()
+        {
+            // To change the databases included in the backup alter the procedure.
+            try
+            {
+                using SqlConnection connection = new(connString);
+                using SqlCommand backup = new("BackupAllDatabases", connection);
+                connection.Open();
+                backup.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        internal static void Backup(string dbName, string path)
+        {
+            DateTime now = DateTime.Now;
+            string timestamp = now.ToString("s").Replace(":", "");
+            string queryStatement = $"BACKUP DATABASE [{dbName}] TO DISK = '{path}\\{dbName}-{timestamp}.bak';";
+
+            Console.Write($"\t{queryStatement}\n");
+            try
+            {
+                using SqlConnection connection = new(connString);
+                using SqlCommand query = new SqlCommand(queryStatement, connection);
+                connection.Open();
+                query.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        internal static void BackupDataBases()
+        {
+            string queryStatement = "SELECT [name]" +
+                ", [database_id]" +
+                ", [create_date]" +
+                ", [physical_database_name] " +
+                "FROM sys.databases " +
+                "WHERE Cast(CASE WHEN name IN ('master', 'model', 'msdb', 'tempdb') THEN 1 " +
+                "ELSE is_distributor END As bit) = 0";
+            try
+            {
+                using SqlConnection connection = new(connString);
+                using SqlCommand query = new SqlCommand(queryStatement, connection);
+                connection.Open();
+                SqlDataReader reader = query.ExecuteReader();
+                while (reader.Read()) 
+                {
+                    Console.WriteLine($"\n{reader[0]}");
+                    // Does folder exist
+                    // if false create
+                    var path = CreateDirectory((string)reader[0]);
+                    // run back up
+                    Backup(reader[0].ToString(), path);
+                }
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+    
+        }
+
+        private static string CreateDirectory(string folder)
+        {
+            string root = $"D:\\\\SQL Backup\\{folder}";
+            if( !Directory.Exists(root) )
+            {
+                Directory.CreateDirectory(root);
+            }
+            return root;
+        }
+    }
+}
+
---
+++ Database Backup/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.Data;
+
+namespace Backup
+{
+    public class Program
+    {
+        private static string _root = $"D:\\SQL Backup";
+        public static void Main()
+        {
+            Console.WriteLine("Starting Backup\n");
+            //
+            Procedure.BackupDataBases();
+
+            var folders = from folder
+                          in Directory.EnumerateDirectories(_root)
+                          select folder;
+            
+            foreach (var folder in folders)
+            {
+                DailyCleanup(folder);
+            }
+
+            Console.WriteLine("\n\nBackup finished");
+        }
+
+        public static void DailyCleanup(string path)
+        {
+            var files = from file
+                        in Directory.EnumerateFiles(path)
+                        select file;
+
+            DateTime today = DateTime.Today;
+            for( int i = 0; i < 10; i++ )
+            {
+                DateTime startDate = today.AddDays( -i );
+
+                var daysFiles = from file
+                                in files
+                                where (
+                                    File.GetCreationTime( file ).Year == startDate.Year
+                                    &&
+                                    File.GetCreationTime( file ).Month == startDate.Month
+                                    &&
+                                    File.GetCreationTime( file ).Day == startDate.Day
+                                )
+                                select file;
+
+                if (daysFiles.Any())
+                {
+                    string oldestFile = files.First();
+                    foreach (var file in daysFiles)
+                    {
+                        if (Directory.GetLastWriteTime(file) > Directory.GetLastWriteTime(oldestFile))
+                        {
+                            oldestFile = file;
+                        }
+                    }
+
+                    var toDelete = from file
+                                   in daysFiles
+                                   where file != oldestFile
+                                   select file;
+
+                    foreach(var file in toDelete)
+                    {
+                        File.Delete(file);
+                    }
+                }
+            }
+        }
+
+        public static void FolderCleanUp()
+        {
+            DateTime today = DateTime.Today;
+            DateTime tenDays = today.AddDays(-10);
+
+            List<string> backupFiles = new List<string>();
+
+            // LIST OUT THE BACK UP SUBFOLDERS
+            var folders = from folder
+                          in Directory.EnumerateDirectories(_root)
+                          select folder;
+
+            foreach (var folder in folders)
+            {
+                Console.WriteLine(folder);
+                // CHECK FOLDERS FOR THE NEWEST FILE
+                var files = from file
+                            in Directory.EnumerateFiles(folder)
+                            where Directory.GetCreationTime(file) >= today
+                            select file;
+
+                // COMPARE ALL FILES AGAINST THE FIRST
+                var max = files.First();
+
+                foreach (var file in files)
+                {
+                    // NEWER FOLDERS ARE ASSIGNED THE MAX REF
+                    if (Directory.GetLastWriteTime(max) <= Directory.GetLastWriteTime(file))
+                    {
+                        max = file;
+                    }
+                }
+
+                // RETURN MAX
+                Console.WriteLine($"{max} - {Directory.GetLastWriteTime(max)}");
+
+                var toDelete = from file
+                               in Directory.EnumerateFiles(folder)
+                               where file != max
+                               select file;
+
+                // DELETE THE OTHER BAK FILES
+                foreach (var file in toDelete)
+                {
+                    Console.WriteLine($"Deleting: {file}");
+                    File.Delete(file);
+                }
+
+                Console.WriteLine("\n\n");
+            }
+        }
+    }
+}
+
